2ced61b513d287d2fbbf9c29e57ca399b4127357
Pedro Beani <bunnyzones25@gmail.com>
2018-07-09 22:31:02 -0300

    Terminando estrutura mundell-fleming

--- src/js/fleming.js
+++ src/js/fleming.js
@@ -3,7 +3,9 @@ $(document).ready( function () {
 
         var module = $('#fleming');
         var classList = ['incial', 'bp', 'lm', 'ls', 'expM', 'expF', 'imp'];
+        var contentMap = {expM: 'money', expF: 'fiscal', imp: 'import'};
         var $view = $('.flemingView, .content', module);
+        var isFlex = false;
 
         var updateView = function(target, rate) {
 
@@ -17,8 +19,15 @@ $(document).ready( function () {
             }
         };
 
+        var updateContent = function(target) {
+            $('.actionItem').addClass('hidden');
+            var selector = '.actionItem.'+ contentMap[target] + '-' + (isFlex ? 'flex' : 'fixed');
+            $(selector).removeClass('hidden');
+        };
+
         $('.flemingItem', module).on('click', function () {
             var target = $(this).attr('plot');
+            updateContent(target);
             updateView(target);
         });
 
@@ -26,8 +35,9 @@ $(document).ready( function () {
             var $this = $(this);
             var rate = $this.find('span').html() ==  'Taxa Fixa' ? 'floating' : '';
             $this.find('span').html(rate == 'floating' ? 'Taxa Flexivel' : 'Taxa Fixa');
-
+            isFlex = rate == 'floating' ? true : false;
             updateView('', rate)
+            updateContent('');
         });
     })();
 });
